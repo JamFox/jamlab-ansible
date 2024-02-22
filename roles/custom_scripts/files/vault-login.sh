000dc0c8b9e06079aac324b353b7691f566d817b
JamFox <github@jamfox.dev>
2024-02-22 15:07:10 +0200

    add custom scripts to all machines

--- roles/custom_scripts/files/vault-login.sh
+++ roles/custom_scripts/files/vault-login.sh
@@ -62,7 +62,7 @@ function update_variable() {
 VAULT_ADDR="${VAULT_ADDR:-https://vault.hpclocal}"
 VAULT_USER=$1
 echo "Logging in to $VAULT_ADDR as $VAULT_USER"
-VAULT_TOKEN=$(vault login -method=userpass username=$VAULT_USER | grep -o 'hvs\.[^[:space:]]*')
+VAULT_TOKEN=$(vault login -address=$VAULT_ADDR -method=userpass username=$VAULT_USER | grep -o 'hvs\.[^[:space:]]*')
 
 update_variable "VAULT_ADDR"
 update_variable "VAULT_USER"
@@ -71,7 +71,7 @@ update_variable "VAULT_TOKEN"
 if [ -n "$VAULT_SSH_KEY" ]; then
   echo "Signing SSH key $VAULT_SSH_KEY"
   VAULT_SIGNED_KEY=$HOME/.ssh/vault_signed.pub
-  vault write -field=signed_key ssh-client-signer/sign/ssh_admin public_key=@${VAULT_SSH_KEY}.pub valid_principals=$VAULT_USER > $VAULT_SIGNED_KEY
+  vault write -address=$VAULT_ADDR -field=signed_key ssh-client-signer/sign/ssh_admin public_key=@${VAULT_SSH_KEY}.pub valid_principals=$VAULT_USER > $VAULT_SIGNED_KEY
   if [ $? -ne 0 ]; then
     echo "Error: failed to sign the SSH key."
     exit 1
