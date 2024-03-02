d5933eb74266dae87523878a6cabca59fa65b011
JamFox <github@jamfox.dev>
2024-03-02 21:07:45 +0200

    actual url and backup

--- roles/custom_scripts/files/backup.sh
+++ roles/custom_scripts/files/backup.sh
@@ -35,3 +35,4 @@ copy_directory() {
 # /mnt/c/Users/jamfox/Downloads/
 copy_latest_file jport /valheim/backups /mnt/d/SynologyDrive/Games/ValheimWords/JamHeim/backups
 copy_directory jport /gickup/backup /mnt/d/SynologyDrive/Repos
+copy_directory jport /actual-data /mnt/d/SynologyDrive/ActualBackup
